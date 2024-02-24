8b87e1b54689ddc28e5304afa81f1789ec0dc319
Kerelko Barabash <kerelkobarabash@gmail.com>
2024-02-24 19:33:32 +0200

    docker-compose demo

--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -1,4 +1,22 @@
+import os
 from .main import *
 
 
 DEBUG = False
+
+ADMINS = [
+	('Kyryl Barabash', 'kerelkobarabash@gmail.com'),
+]
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
