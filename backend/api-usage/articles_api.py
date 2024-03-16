6b09b252dc47deda2ed451382ee76ffdb41c4c4c
Kerelko Barabash <kerelkobarabash@gmail.com>
2024-03-16 14:54:27 +0200

    api updated

--- backend/api-usage/articles_api.py
+++ backend/api-usage/articles_api.py
@@ -1,5 +1,4 @@
 import os
-import requests
 from dotenv import load_dotenv
 
 from utils import get_response_or_error
@@ -25,42 +24,12 @@ def get_article_list() -> list[dict]:
 
 def get_hyperlinked_article_list() -> list[dict]:
 	url = BASE_ARTICLE_URL + 'hyperlinked/'
-	try:  
-		response = requests.get(
-			url, 
-			timeout=5
-		)
-		response.raise_for_status()
-	except requests.Timeout as ex:
-		raise requests.Timeout(
-			f'Timeout error occurred: Request took longer than the specified' 
-			f'timeout period, url: {url}'
-		) from ex
-	except requests.HTTPError as ex:
-		raise requests.HTTPError(
-			f'HTTP error occurred while getting response from: {url}'
-		) from ex
+	response = get_response_or_error(url)
 	return response.json()
 
 
 def get_topic_list() -> list[dict]:
 	url = BASE_TOPIC_URL
-	try:
-		response = requests.get(
-			url,
-			timeout=5
-		)
-		response.raise_for_status()
-	except requests.Timeout as ex:
-		raise requests.Timeout(
-			f'Timeout error occurred: Request took longer than the specified' 
-			f'timeout period, url: {url}'
-		) from ex
-	except requests.HTTPError as ex:
-		raise requests.HTTPError(
-			f'HTTP error occurred while getting response from: {url}'
-		) from ex
+	response = get_response_or_error(url)
 	return response.json()
 	
-
-print(get_article_list())
